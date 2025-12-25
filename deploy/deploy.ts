35b87d092799d3b06704a5843ad4acb64068f5b6
matthewgrantz <StempleGraybeal28041977rk@hotmail.com>
2025-12-25 12:25:33 +0800

    Wire deploy script for EncryptedSurvey

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedSurvey = await deploy("EncryptedSurvey", {
     from: deployer,
     log: true,
+    gasLimit: 8_000_000,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedSurvey contract: `, deployedSurvey.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encryptedSurvey"; // id required to prevent reexecution
+func.tags = ["EncryptedSurvey"];
